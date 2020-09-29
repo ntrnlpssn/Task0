35fa2ac47a8108f5d296a54777f6e0faac08685a
ntrnlpssn <ntrnlpssn@gmail.com>
2020-09-29 17:43:56 +0300

    First version

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    double *values,MaxDouble=0;
+    int size;
+    cout<<"Enter a size of array: " << "\n";
+    cin>>size;
+    values = new double[size];
+
+    for(int i=0 ; i<size; i++)
+    {
+        values[i] = (rand() % 1000)/100.0+0.01;
+        cout << values[i] << " ";
+    }
+
+    cout <<"\n";
+
+    for(int i=0 ; i<size; i++)
+    {
+        if(MaxDouble < values[i])
+            MaxDouble =values[i];
+    }
+
+    cout <<"Max value of array: " << MaxDouble <<"\n" ;
+
+  system("PAUSE");
 
     return a.exec();
 }
